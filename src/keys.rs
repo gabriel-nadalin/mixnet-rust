837c4916b9387f1b765d1987dd0641ac622454c4
gabriel-nadalin <gknadalin@gmail.com>
2025-09-03 01:56:05 -0300

    add e2easy.rs, keys.rs

---
+++ src/keys.rs
@@ -0,0 +1,27 @@
+pub struct SecretKey {
+
+}
+
+impl SecretKey {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn decrypt() {
+        todo!()
+    }
+
+    pub fn public_key() -> PublicKey {
+        todo!()
+    }
+}
+
+pub struct PublicKey {
+
+}
+
+impl PublicKey {
+    pub fn encrypt() {
+        todo!()
+    }
+}
